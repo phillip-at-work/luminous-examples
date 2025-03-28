efd16bdba29bbf803e71b590c930020f2a515095
Phillip Curtsmith <phillip.curtsmith@str.us>
2025-03-28 08:32:08 -0400

    updated two camera example

--- two_detectors.py
+++ two_detectors.py
@@ -13,12 +13,12 @@ scene += Isotropic(position=Vector(5, 5, -10), pointing_direction=Vector(0, 0, 1
 camera_one = Camera(width=400, height=300, position=Vector(0, 0.35, -1), pointing_direction=Vector(0, 0, 1))
 scene += camera_one
 
-camera_two = Camera(width=400, height=300, position=Vector(0, 0.35, 6), pointing_direction=Vector(0, 0, -1))
+camera_two = Camera(width=400, height=300, position=Vector(0, 2, -1), pointing_direction=Vector(0, 3, 1))
 scene += camera_two
 
-scene += Sphere(center=Vector(0.75, 0.1, 1), radius=0.6, color=Vector(0, 0, 1))
-scene += Sphere(center=Vector(-0.75, 0.1, 2.25), radius=0.6, color=Vector(0.5, 0.223, 0.5))
-scene += Sphere(center=Vector(-2.75, 0.1, 3.5), radius=0.6, color=Vector(1, 0.572, 0.184))
+scene += Sphere(center=Vector(0.75, 0.1, 1), radius=0.6, color=Vector(1, 0, 0))
+scene += Sphere(center=Vector(-0.75, 0.1, 2.25), radius=0.6, color=Vector(0, 1, 0))
+scene += Sphere(center=Vector(-2.75, 0.1, 3.5), radius=0.6, color=Vector(0, 0, 1))
 scene += CheckeredSphere(center=Vector(0, -99999.5, 0), radius=99999, color=Vector(0.75, 0.75, 0.75), reflectance=0.25)
 
 scene.raytrace()
