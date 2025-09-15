4931a4a8c7bef1363ce12916d54ead41be1c9258
Phillip Curtsmith <phillip.curtsmith@str.us>
2025-09-14 21:10:23 -0400

    added example to demosntrate multiple sources

---
+++ attach_ray_debugger_2_sources.py
@@ -0,0 +1,30 @@
+from matplotlib import pyplot as plt
+
+from luminous.src.math.vector import Vector
+from luminous.src.scene.scene import Scene
+from luminous.src.element.element import Sphere
+from luminous.src.detector.detector import Camera
+from luminous.src.source.source import Isotropic
+
+scene = Scene(log_level=10, log_file="./results/luminous.log")
+
+scene.attach_ray_debugger(path="./results", filename="debug_ray_trace")
+
+scene += Isotropic(position=Vector(5, 5, -10), pointing_direction=Vector(0, 0, 1))
+scene += Isotropic(position=Vector(0, -5, -2), pointing_direction=Vector(0, 0, 1))
+
+camera = Camera(width=200, height=150, position=Vector(0, 0.35, -1), pointing_direction=Vector(0, 0, 1))
+scene += camera
+
+scene += Sphere(center=Vector(0.75, 0.1, 1), radius=0.6, color=Vector(0, 0, 1), user_params={'specular':0.25, 'n_s':10})
+scene += Sphere(center=Vector(0, 0, 3.5), radius=0.6, color=Vector(1, 0.572, 0.184), user_params={'specular':0.25, 'n_s':10})
+
+scene.raytrace()
+
+image = camera.view_data()
+
+plt.imshow(image)
+p = "./results/attach_ray_debugger_2_sources_example.png"
+print(f"attach_ray_debugger_2_sources example plot saved to: {p}")
+plt.savefig(p)
+plt.close()
